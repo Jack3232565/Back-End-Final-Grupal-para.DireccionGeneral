5a4a2e21ed21d914cba86d6301e7e75a9308a826
Carlos.Crespo <210237@utxicotepec.edu.com>
2024-08-22 06:23:16 -0600

    quitando permisos a rol

--- routes/rol.py
+++ routes/rol.py
@@ -24,28 +24,28 @@ def read_rols(db: Session = Depends(get_db)):
     db_rols= crud.rols.get_rols(db=db)
     return db_rols
 
-@rol.post("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"], dependencies=[Depends(Portador())])
+@rol.post("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"])
 def read_rol(id: int, db: Session = Depends(get_db)):
     db_rol= crud.rols.get_rol(db=db, id=id)
     if db_rol is None:
         raise HTTPException(status_code=404, detail="Rol not found")
     return db_rol
 
-@rol.post("/rols/", response_model=schemas.rols.Rol, tags=["Roles"], dependencies=[Depends(Portador())])
+@rol.post("/rols/", response_model=schemas.rols.Rol, tags=["Roles"])
 def create_rol(rol: schemas.rols.RolCreate, db: Session = Depends(get_db)):
     db_rol = crud.rols.get_rol_by_nombre(db, nombre=rol.Nombre)
     if db_rol:
         raise HTTPException(status_code=400, detail="Rol existente intenta nuevamente")
     return crud.rols.create_rol(db=db, rol=rol)
 
-@rol.put("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"], dependencies=[Depends(Portador())])
+@rol.put("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"])
 def update_rol(id: int, rol: schemas.rols.RolUpdate, db: Session = Depends(get_db)):
     db_rol = crud.rols.update_rol(db=db, id=id, rol=rol)
     if db_rol is None:
         raise HTTPException(status_code=404, detail="Usuario no existe, no actualizado")
     return db_rol
 
-@rol.delete("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"], dependencies=[Depends(Portador())])
+@rol.delete("/rol/{id}", response_model=schemas.rols.Rol, tags=["Roles"])
 def delete_rol(id: int, db: Session = Depends(get_db)):
     db_rol = crud.rols.delete_rol(db=db, id=id)
     if db_rol is None:
